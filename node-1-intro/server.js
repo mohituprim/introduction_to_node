f6535e9efcd62123a270aed6961d1b727e25a00b
mohituprim <mohituprim@gmail.com>
2017-06-02 15:51:57 +0000

    CallBack Example

--- node-1-intro/server.js
+++ node-1-intro/server.js
@@ -5,6 +5,5 @@ http.createServer(function (req, res) {
     res.end('Hello world\n');
 }).listen(process.env.PORT, process.env.IP);
 
-// https://ps-intro-to-node-mickish.c9users.io/
 // Server running at 0.0.0.0:8080
 console.log('Server running at '+process.env.IP+':'+process.env.PORT);
